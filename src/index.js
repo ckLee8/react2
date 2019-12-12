5cd2c9e1adad2fc588c2f64adbb40235c17add20
ckLee <ckLee@ckLees-MBP.fios-router.home>
2019-12-12 02:44:09 -0500

    add comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-// Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
-// Create a react component
-const App = ()=> {
-    const buttonText = 'Click Me!'
-    return <div>
-        <label className="label" for="name">Enter Name:</label>
-        <input id="name" type="text" />
-<button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>çç
-    </div>
-}
+import CommentDetail from './CommentDetail'
 
-//Take the react component and show it
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+const App = () => {
+    return(
+        <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
